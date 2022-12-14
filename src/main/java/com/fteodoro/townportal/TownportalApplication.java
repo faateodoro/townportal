62c37b1f3842acbf174693fef8dae04e6ab22210
Fabiano Teodoro <faateodoro@gmail.com>
2022-12-14 18:11:27 -0300

    Commit inicial

---
+++ src/main/java/com/fteodoro/townportal/TownportalApplication.java
@@ -0,0 +1,13 @@
+package com.fteodoro.townportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TownportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TownportalApplication.class, args);
+	}
+
+}
